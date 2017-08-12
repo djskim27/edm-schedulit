2fc05839e23fd7d3ac69bb3f356706ed3babae18
djskim27 <djs.kim27@gmail.com>
2017-08-12 19:22:02 -0400

    got data to map

--- client/src/components/ShowList.jsx
+++ client/src/components/ShowList.jsx
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 import Show from './Show'
 
 class ShowList extends Component {
+    
     render(){
+        const shows = this.props.shows;
+        const showComponent = shows.map((show, i) => {
+           return <Show show = {show}/>;
+        })
         return(
             <div>
-                Hello from ShowList
-                <Show />
+                <h1>EDM Shows</h1>
+                {showComponent}
+                
             </div>
         )
     }
