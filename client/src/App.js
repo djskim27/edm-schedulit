2cf7b21c643e579c8b67cf6069e441547e759923
djskim27 <djs.kim27@gmail.com>
2017-08-12 15:07:35 -0400

    started react components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-
+import Homepage from './components/Homepage'
 
 class App extends Component {
   render() {
     return (
       <div>
-        
+        <Homepage />
       </div>
     );
   }
