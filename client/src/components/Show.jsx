85777028bbea426c79396255d960ef0a2ef3a42f
djskim27 <djs.kim27@gmail.com>
2017-08-15 22:54:56 -0400

    getting ready for HEROKU

--- client/src/components/Show.jsx
+++ client/src/components/Show.jsx
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import {Redirect} from 'react-router-dom';
 import styled from 'styled-components';
 
 
 class Show extends Component {
-    constructor(){
+    constructor() {
         super();
         this.state = {
-            id: "",
+            id: '',
             name: '',
             city: '',
             state: '',
             venue: '',
             eventPage: '',
             artistImage: '',
-            userId:''
-            
+            userId: ''
+
         }
     }
 
-    componentWillMount(){
+    componentWillMount() {
         const id = this.props.match.params.showId;
         axios.get(`/api/shows/${id}`).then((res) => {
             console.log(res.data.artistList[0].imgUrl)
@@ -30,38 +31,43 @@ class Show extends Component {
                 state: res.data.state,
                 venue: res.data.venue,
                 eventPage: res.data.eventPage,
-                artistImage: res.data.artistList[0].imgUrl
+                artistImage: res.data.artistList[0].imgUrl,
+                redirect: false
             })
-            
-        }).catch((err)=> {
+
+        }).catch((err) => {
             console.log(err);
         })
     }
     _addEventToUser = (e) => {
         e.preventDefault();
-        axios.post(`/api/user/${this.props.user._id}/add/${this.props.match.params.showId}`).then((res)=>{
-            this.setState
-        }).catch((err)=>{
+        axios.post(`/api/user/${this.props.user._id}/add/${this.props.match.params.showId}`).then((res) => {
+            this.setState({ redirect: true });
+        }).catch((err) => {
             console.log(err);
         })
     }
 
-    render(){
-        
-   
-        return(
-            <div>
+    render() {
 
-                <img src={this.state.artistImage} />
-                 
-                <h3>Name: {this.state.name}</h3>
-                <p>Location: {this.state.venue} in {this.state.city}, {this.state.state} </p>
-                <a href={this.state.eventPage} target='blank'>Buy Tickets</a>
-                {this.props.loggedIn ? <button onClick={this._addEventToUser}>Add Event</button>:null}
+        if (this.state.redirect) {
+            return <Redirect to={`/user/${this.props.user._id}/shows`} />
+        } else {
+
+            return (
+                <div>
+
+                    <img src={this.state.artistImage} />
+
+                    <h3>Name: {this.state.name}</h3>
+                    <p>Location: {this.state.venue} in {this.state.city}, {this.state.state} </p>
+                    <a href={this.state.eventPage} target='blank'>Buy Tickets</a>
+                    {this.props.loggedIn ? <button onClick={this._addEventToUser}>Add Event</button> : null}
 
 
-            </div>
-        )
+                </div>
+            )
+        }
     }
 }
 
