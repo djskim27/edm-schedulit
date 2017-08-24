be6d430a988ace2731d8aa1a6f676be8390a931b
djskim27 <djs.kim27@gmail.com>
2017-08-23 23:38:35 -0400

    removed  sign up / log out text decoration

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,6 @@ import {Button, Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstr
 import styled from 'styled-components';
 
 
-
 class App extends Component {
   constructor(){
         super();
@@ -34,7 +33,6 @@ componentWillMount(){
 _updateUsers = (data) => {
   this.setState({users: data})
 }
-
 
 
 _logIn = () => {
@@ -72,99 +70,93 @@ _logInAndPassNewUser = (user) => {
       <div>
         {this.state.loggedIn ? 
         <div>
-        <Navbar inverse collapseOnSelect fixedTop >
-          <Navbar.Header>
-         
-            <Navbar.Brand>
-           <Link to={`/user/${this.state.user._id}/shows`}>Home</Link>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-          </Navbar.Header>
-          <Navbar.Collapse>
-        
-            <Nav pullRight>
-              <Navbar.Brand><Link to={`/user/${this.state.user._id}`} style={{textDecoration:'none'}}>{this.state.user.userName}</Link></Navbar.Brand>
-              <Navbar.Brand><Link to="/" onClick={this._logOut} style={{textDecoration:'none'}}>Log Out</Link> </Navbar.Brand>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-        </div> : 
-        <div>
-        <Navbar inverse collapseOnSelect fixedTop>
-          <Navbar.Header>
-         
-            <Navbar.Brand>
-           <Link to="/">Home</Link>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-          </Navbar.Header>
-          <Navbar.Collapse>
-        
-            <Nav pullRight>
-               <Navbar.Brand><Link to="/login" style={{textDecoration:'none'}}>Login</Link></Navbar.Brand>
-              <Navbar.Brand> <Link to="/signup" style={{textDecoration:'none'}}>Sign Up</Link> </Navbar.Brand>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
+          <Navbar inverse collapseOnSelect fixedTop >
+            <Navbar.Header>
+          
+              <Navbar.Brand>
+            <Link to={`/user/${this.state.user._id}/shows`}>Home</Link>
+              </Navbar.Brand>
+              <Navbar.Toggle />
+            </Navbar.Header>
+            <Navbar.Collapse>
+          
+              <Nav pullRight>
+                <Navbar.Brand><Link to={`/user/${this.state.user._id}`} style={{textDecoration:'none'}}>{this.state.user.userName}</Link></Navbar.Brand>
+                <Navbar.Brand><Link to="/" onClick={this._logOut} style={{textDecoration:'none'}}>Log Out</Link></Navbar.Brand>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
         </div> 
-        
-        }
+        : 
+        <div>
+          <Navbar inverse collapseOnSelect fixedTop>
+            <Navbar.Header>
+          
+              <Navbar.Brand>
+            <Link to="/">Home</Link>
+              </Navbar.Brand>
+              <Navbar.Toggle />
+            </Navbar.Header>
+            <Navbar.Collapse>
+          
+              <Nav pullRight>
+                <Navbar.Brand><Link to="/login" style={{textDecoration:'none'}}>Login</Link></Navbar.Brand>
+                <Navbar.Brand><Link to="/signup" style={{textDecoration:'none'}}>Sign Up</Link></Navbar.Brand>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </div> }
          
           <Route exact path="/" render={routeProps => 
           <Homepage {...routeProps}
             users={this.state.users}
             loggedIn={this.state.loggedIn}
             updateUsers={this._updateUsers}
-            />} />
+          />} />
 
           <Route exact path="/signup" render={routeProps => 
           <Signup {...routeProps}
             login={this._logInAndPassNewUser}
           
-          />}/>
+          />} />
 
           <Route exact path="/login" render={routeProps => 
           <Login {...routeProps}
             users={this.state.users}
             loggedIn={this.state.loggedIn}
             changeLogIn={this._logIn}
-            />} />
+          />} />
 
           <Route exact path="/show/:showId" render={routeProps=>
           <Show {...routeProps}
-          loggedIn={this.state.loggedIn}
-          user={this.state.user}
-          />}/>
+            loggedIn={this.state.loggedIn}
+            user={this.state.user}
+          />} />
 
           <Route exact path="/user/:userId" render={routeProps =>
            <UserProfile {...routeProps}
            user={this.state.user}
            updateUsers={this.state._updateUsers}
            logOut={this._logOut}
-           />} />
+          />} />
 
            <Route exact path="/user/:userId/edit" render={routeProps =>
            <EditUserForm {...routeProps}
-           user={this.state.user}
-           setLoggedInUser={this._setLoggedInUser}
-           
-           />} />
-
+            user={this.state.user}
+            setLoggedInUser={this._setLoggedInUser}
+          />} />
 
           <Route exact path="/user/:userId/shows" render={routeProps => 
           <Homepage {...routeProps}
             loggedIn={this.state.loggedIn}
             setLoggedInUser={this._setLoggedInUser}
             updateUsers={this._updateUsers}
-            />} />
+          />} />
           
       </div>
+
     </Router>
-     
     
-       
-   
-  
     );
   }
 }
